4c19d3e244d4e79976c1518533477de4f0ae9602
RadyaI <radyaiftikhar@gmail.com>
2025-10-27 08:08:44 +0700

    d

--- Modul_3/tugas2/src/MainApp.java
+++ Modul_3/tugas2/src/MainApp.java
@@ -23,7 +23,6 @@ public class MainApp {
         System.out.println("===== SPLIT BILL APP =====");
         System.out.println("1. Sama Rata");
         System.out.println("2. Sesuai Porsi");
-        System.out.println("3. Keluar");
         System.out.print("Pilih mode (1/2/3): ");
         int mode = input.nextInt();
         input.nextLine();
